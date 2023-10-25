5ebbb13bda3500c87a90732bdde3f714187ec7f0
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2023-10-25 11:58:19 -0600

    agrega nuevos archivos

---
+++ Expolitec/Inicio.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <Title>Inicio</Title>
+        <link rel="stylesheet" href="inicio.css">
+    </head>
+    <body>
+        <header></header>
+        <aside id="publicidad">
+            <img id=im src="imgs/img1.jpeg">
+        </aside>
+        <main>
+            <article id="paquetes">
+                <h1 class="tit">Paquetes de habitaciones</h1><br>
+                <section class="habitacion">
+                    <img class=hab src="imgs/img2.jpeg">
+                </section>
+                <section class="habitacion">
+                    <img class=hab src="imgs/img2.jpeg">
+                </section>
+                <section class="habitacion">
+                    <img class=hab src="imgs/img2.jpeg">
+                </section>
+                <section class="habitacion2">
+                    <img class=hab src="imgs/img2.jpeg">
+                </section>
+            </article>
+        </main>
+    </body>
+</html>
