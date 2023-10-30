21ff71e80c29be9b9a31a77a8b30391584116231
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2023-10-29 18:10:59 -0600

    Maquetación completa de la pantalla de inicio

--- Expolitec/Inicio.php
+++ Expolitec/Inicio.php
@@ -3,29 +3,93 @@
     <head>
         <meta charset="UTF-8">
         <Title>Inicio</Title>
-        <link rel="stylesheet" href="inicio.css">
+        <link rel="stylesheet" href="inicioP.css">
+        <link rel="stylesheet" href="barra_nav_foot.css">
     </head>
     <body>
-        <header></header>
-        <aside id="publicidad">
-            <img id=im src="imgs/img1.jpeg">
-        </aside>
-        <main>
-            <article id="paquetes">
-                <h1 class="tit">Paquetes de habitaciones</h1><br>
-                <section class="habitacion">
-                    <img class=hab src="imgs/img2.jpeg">
+        <header>
+            <article id="titfo">
+                <section id="titC">
+                    <br>
+                    <h1 id="tit">PawTel</h1>
                 </section>
-                <section class="habitacion">
-                    <img class=hab src="imgs/img2.jpeg">
+                <section id="search">
+                    <br><br><br>
+                    <input type="search" id="sear">
                 </section>
-                <section class="habitacion">
-                    <img class=hab src="imgs/img2.jpeg">
-                </section>
-                <section class="habitacion2">
-                    <img class=hab src="imgs/img2.jpeg">
+                <section id="perfil">
+                    <div id="pic"></div>
                 </section>
             </article>
+            <section id="bnav">
+                <nav class="nave">
+                    <ul class="menu">
+                        <li>Inicio</li>
+                        <li>Paquetes</li>
+                        <li>Servicios</li>
+                        <li>Reservaciones</li>
+                    </ul>
+
+                </nav>
+            </section>
+            
+        </header>
+
+        <aside id="publicidad"><!-- Esta sección es para colocar una imagen de publicidad-->
+
+        </aside>
+        <main>
+            <article id="paquetes"><!--Toda esta parte contendrá los diferentes tipos de habitaciones-->
+                <br>
+                <h1 class="tits">Planes de Habitaciones</h1>
+                <section id="container-rooms">
+                    <section class="habitacion"></section>
+                    <section class="habitacion"></section>
+                    <section class="habitacion"></section>
+                    <section class="habitacion2"></section>
+                </section>
+            </article><!-------------------------------------------------------------------------------->
+
+            <aside class="imagen"></aside> <!--Imagen de publicidad-->
+
+
+            <article class="servicios"> <!--Esta parte contendrá una vista rapid de los serviios que ofrece el hotel-->
+                <br><br> 
+                <h1 class="titc">Servicios</h1>
+                <section id="container-rooms">
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                </section>
+            </article><!-------------------------------------------------------------------------------->
+
+            <aside class="imagen"></aside><!--imagen de publicidad-->
+
+            <article class="servicios"> <!--Esta parte contendrá una vista rapida de las actividades existentes en el hotel-->
+                <br><br> 
+                <h1 class="titc">Actividades</h1>
+                <p></p>
+                <section id="container-rooms">
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <section class="square_s"></section>
+                    <br><br>
+                    <button id="but">Reserva Ahora</button>
+                </section>
+            </article><!------------------------------------------------------------------------------->
+
+            <footer></footer>
         </main>
     </body>
 </html>
