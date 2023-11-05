6b3a6436a12994ab5eceeeda29064757a8f15040
ferconh <143868890+ferconh@users.noreply.github.com>
2023-11-05 01:54:03 -0600

    reserva css

--- Expolitec/reserva-resta.php
+++ Expolitec/reserva-resta.php
@@ -1,11 +1,11 @@
 
-<?php
+<?php/*
 session_start();
 if (!isset($_SESSION['loggedin'])) {
 
     header('Location: Login.html');
     exit;
-}
+}*/
 
 ?>
 
@@ -14,29 +14,35 @@ if (!isset($_SESSION['loggedin'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reserva-resta.css">
     <title>Pawtel</title>
 </head>
 <body>
-    <head>
+    <header>
         <h1>Restaurante food dog</h1>
-    </head>
+    </header>
     <main> 
-        <form action="save-reservarest.php" method="post">
-        <label for="nombre">Nombre (Es el nombre cual se va quedar la reserva)</label>
-        <input type="text" name="nombre" class="inputs" required>
-        <label for="correo">Correo:</label>
-        <input type="email" name="correo" class="inputs" required>
-        <label for="hora">Hora de la reservacion</label>
-        <input type="time" name="hora" class="inputs" required>
-        <label for="personas">No. Personas</label>
-        <input type="number" name="personas" class="inputs"required>
-        <label for="telefono">Telefono:</label>
-        <input type="tel" name="telefono" class="inputs"required>
-        <label for="celebracion">¿Que celebras?</q></label>
-        <input type="text" name="celebracion" class="inputs">
-        <p>Importante: Solo tendrás 10 min. de tolerancia.</p>
-        <input type="submit" value="Reservar">
-        </form>
+        <section class="cont">
+            <section class="form">
+                <form action="save-reservarest.php" method="post">
+                    <label for="nombre">Nombre Completo:</label><br>
+                    <input type="text" name="nombre" class="inputs" required><br>
+                    <label for="correo">Correo:</label><br>
+                    <input type="email" name="correo" class="inputs" required><br>
+                    <!--Falto la Fecha :(((-->
+                    <label for="hora">Hora de la reservacion</label><br>
+                    <input type="time" name="hora" class="inputs" required><br>
+                    <label for="personas">No. Personas</label><br>
+                    <input type="number" name="personas" class="inputs"required><br>
+                    <label for="telefono">Telefono:</label><br>
+                    <input type="tel" name="telefono" class="inputs"required><br>
+                    <label for="celebracion">¿Que celebras?</q></label><br>
+                    <input type="text" name="celebracion" class="inputs"><br>
+                    <p>Importante: Solo tendrás 10 min. de tolerancia.</p>
+                    <input type="submit" value="Reservar" class="reservar">
+                </form>
+            </section>
+        </section>
     </main>
 </body>
 </html>
