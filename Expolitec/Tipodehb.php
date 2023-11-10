e2e5ba17be220a573c3004ca709b4830f487a8f8
Kazu000001 <emmanuel.gonzalez1438@alumnos.udg.mx>
2023-11-09 22:11:23 -0600

    sii

---
+++ Expolitec/Tipodehb.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,th,td{
+            border: 1px solid;
+        }
+        table{
+            border-collapse: collapse;
+            width: 80%;
+        }
+    </style>
+</head>
+<body>
+    <h2>Tipo de habitaciones</h2>
+    <form action="" method="post">
+        <label for="campo">Buscar:</label>
+        <input type="text" name="campo" id="campo">
+    </form>
+    <table>
+        <thead>
+            <th>idh</th>
+            <th>Imagen</th>
+            <th>Nombre habitacion</th>
+            <th>Personas</th>
+            <th>Duchas</th>
+            <th>Camas</th>
+            <th>Precio</th>
+            <th>Descrpt 1</th>
+            <th>Personas 2</th>
+            <th>Duchas 2</th>
+            <th>Camas 2</th>
+            <th>Precio 2</th>
+            <th>Descrpt 2</th>
+            <th>No. habitacion</th>
+            <th>No. habitacion disponibles</th>
+            <th>Extras</th>
+        </thead>
+
+        <tbody id="contend">
+
+        </tbody>
+
+
+    </table>
+</body>
+</html>
---
+++ Expolitec/load.php
@@ -0,0 +1,5 @@
+<?php
+require('conexion.php');
+
+$columas=[ 'idh','Imagen','nomh', 'pers','duch','cams','precio','descriph1','pers2','duch2','cams2','precio2','descriph2','no_h','dispo', 'extras'];
+?>
