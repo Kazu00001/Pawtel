8cb5c5c452839612a796203e6f7b652a0f24a07b
ferconh <143868890+ferconh@users.noreply.github.com>
2023-11-21 00:28:14 -0600

    Empleado

---
+++ Expolitec/p-empleado.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="p-empleado.css">
+    <link rel="stylesheet" href="barra_nav_foot.css">
+</head>
+<body>
+    <header>
+        <article id="titfo">
+            <section id="titC">
+                <br>
+                <a href="Inicio.php"><h1 id="tit">PawTel</h1></a>
+                </section>
+                <section id="search">
+                    <br><br><br>
+                    <input type="search" id="sear">
+                </section>
+                <section id="perfil">
+                <div id="pic"></div>
+                </section>
+        </article>
+    </header>
+    <main>
+        <article class="emp">
+            <section class="perf">
+            <section id="fotop">
+                <div id="pict"></div>
+                </section>
+                <section class="nome"><h1 id="name">Nombre de Empleado</h1></section>
+            </section>
+            <section class="data">
+                <section class="data2">
+                    <section class="detail">
+                        <h1 id="Detal">Detalles del Empleado</h1>
+                        <p class="dat">Edad:</p>
+                        <p class="dat">Fecha de Nacimiento:</p>
+                        <p class="dat">País:</p>
+                        <p class="dat">ciudad:</p>
+                    </section>
+                    <section class="detaile">
+                        <p class="dat">Email:</p>
+                        <p class="dat">Numero Telefonico:</p>
+                        <p class="dat">Horario de Trabajo:</p>
+                        <p class="dat">Puesto de Trabajo:</p>
+                        <p class="dat">Cuenta Bancaria:</p>
+                        <p class="dat">Salario Mensual:</p>
+                    </section>
+                </section>
+            </section>
+        </article>
+    </main>
+</body>
+</html>
