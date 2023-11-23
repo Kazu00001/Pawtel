9f3c45577d228241116b8b06f0ac98a72a2cd0ef
ferconh <143868890+ferconh@users.noreply.github.com>
2023-11-22 22:58:24 -0600

    Habitación y usser

--- Expolitec/Tipodehb.php
+++ Expolitec/Tipodehb.php
@@ -3,63 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        table, th, td {
-            border: 1px solid;
-        }
-        table {
-            border-collapse: collapse;
-            width: 80%;
-        }
-    </style>
+    <link rel="stylesheet" href="admin_bar_foot.css">
+    <link rel="stylesheet" href="Tipodehb.css">
+    <title>Habitaciones</title>
 </head>
 <body>
-    <h2>Tipo de habitaciones</h2>
-    <form action="" method="post">
-        <label for="campo">Buscar:</label>
-        <input type="text" name="campo" id="campo" onkeyup="getData()">
-    </form>
-    <table>
-        <thead>
-            <th>idh</th>
-            <th>Imagen</th>
-            <th>Nombre habitacion</th>
-            <th>Personas</th>
-            <th>Duchas</th>
-            <th>Camas</th>
-            <th>Precio</th>
-            <th>Descrpt 1</th>
-            <th>Personas 2</th>
-            <th>Duchas 2</th>
-            <th>Camas 2</th>
-            <th>Precio 2</th>
-            <th>Descrpt 2</th>
-            <th>No. habitacion</th>
-            <th>No. habitacion disponibles</th>
-            <th>Extras</th>
-        </thead>
-        <tbody id="content"></tbody>
-        <script>
-            document.addEventListener("DOMContentLoaded", getData);
+<header>
+        <article id="titfo">
+            <section id="titC">
+                <br>
+                <a href="Inicio.php"><h1 id="tit">PawTel</h1></a>
+                </section>
+                <section id="search">
+                    <br><br><br>
+                    <input type="search" id="sear">
+                </section>
+                <section id="perfil">
+                <div id="pic"></div>
+                </section>
+        </article>
+    </header>
+    <main>
+        <article id="title">
+            <h2>Tipo de habitaciones</h2>
+        </article>
+        <section class="busq">
+            <form action="" method="post" class="busqe">
+                <label for="campo" class="texto">Buscar:</label>
+                <input type="text" name="campo" id="campo" class="texto" onkeyup="getData()" placeholder="Habitaciones">
+            </form>
+        </section>
+        <section id=container>
+        <table>
+            <thead>
+                <th>id</th>
+                <th>Imagen</th>
+                <th>Nombre habitacion</th>
+                <th>Personas</th>
+                <th>Duchas</th>
+                <th>Camas</th>
+                <th>Precio</th>
+                <th>Descrpt 1</th>
+                <th>Personas 2</th>
+                <th>Duchas 2</th>
+                <th>Camas 2</th>
+                <th>Precio 2</th>
+                <th>Descrpt 2</th>
+                <th>No. habitacion</th>
+                <th>No. habitacion disponibles</th>
+                <th>Extras</th>
+                <th></th>
+                <th></th>
+            </thead>
+            <tbody id="content"></tbody>
+            <script>
+                document.addEventListener("DOMContentLoaded", getData);
 
-            function getData() {
-                let input = document.getElementById("campo").value;
-                let content = document.getElementById("content");
-                let url = "load.php";
-                let formData = new FormData();
-                formData.append('campo', input);
+                function getData() {
+                    let input = document.getElementById("campo").value;
+                    let content = document.getElementById("content");
+                    let url = "load.php";
+                    let formData = new FormData();
+                    formData.append('campo', input);
 
-                fetch(url, {
-                    method: "POST",
-                    body: formData
-                }).then(response => response.text())
-                    .then(data => {
-                        console.log(data);
-                        content.innerHTML = data;
-                    }).catch(err => console.log(err));
-            }
-        </script>
-    </table>
+                    fetch(url, {
+                        method: "POST",
+                        body: formData
+                    }).then(response => response.text())
+                        .then(data => {
+                            console.log(data);
+                            content.innerHTML = data;
+                        }).catch(err => console.log(err));
+                }
+            </script>
+        </table>
+            </section>
+    </main>
 </body>
 </html>
