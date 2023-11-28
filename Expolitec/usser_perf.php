2c7f2719a87f37fa306c61af04fcf69994998c16
ferconh <143868890+ferconh@users.noreply.github.com>
2023-11-28 06:52:31 -0600

    try perfil

---
+++ Expolitec/usser_perf.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="usser_perf.css">
+    <link rel="stylesheet" href="barra_nav_foot.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+<header>
+            <article id="titfo">
+                <section id="titC">
+                    <br>
+                    <a href="inicio.php"><h1 id="tit">PawTel</h1></a>
+                </section>
+                <section id="barNav">
+                <nav class="nave">
+                    <ul class="menu">
+                        <li><a href="inicio.php">Inicio</a></li>
+                        <li><a href="#">Servicios</a>
+                            <div class="topM"></div>
+                            <ul class="menuv">
+                                <li class="list"><a href="cafeteria.php">Cafeteria</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Reservaciones</a>
+                            <div class="topM"></div>
+                            <ul class="menuv">
+                                <li class="list"><a href="reservas.php">Reserva Hotel</a></li>
+                                <li class="list"><a href="Reserva-resta.php">Reserva Restaurante</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </nav>
+                </section>
+                <section id="perfil">
+                    <div id="pic"></div>
+                </section>
+            </article>
+        </header>
+        <main>
+            <article class="left">
+                <section class="reser">
+                    <!--<section id="con1">
+                        <section id="first"><h1 class="text">Tus Reservas<br><hr class="line"></h1></section>
+                    </section>-->
+                    
+                </section>
+            </article>
+            <article id="perf">
+                
+                <!--<section id="per">
+                <section class="photo"></section>
+                </section>-->
+            </article>
+        </main>
+</body>
+</html>
