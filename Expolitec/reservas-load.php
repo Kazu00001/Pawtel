a9cc8dd4f9b0f477bb0af866c968750c4b5785d0
Kazu000001 <emmanuel.gonzalez1438@alumnos.udg.mx>
2023-11-30 05:17:10 -0600

    buttons

--- Expolitec/reservas-load.php
+++ Expolitec/reservas-load.php
@@ -65,8 +65,20 @@ if ($num_rows > 0) {
         </script>
         </button>'
         ;
-        echo '<td> <a href="">Editar</a></td>';
-        echo '</tr>';
+        echo '<td> <form id="edit"> 
+        <button class="button">
+            Apply Now
+            <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+                <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+            </svg>
+            </button>
+                    </form> </td> <script>
+                    function enviarFormulario3() {
+                        document.getElementById("edit").submit();
+                    }
+                    </script>'
+                    ;
+                    echo '</tr>';
     }
 } else {
     echo '<tr>';
