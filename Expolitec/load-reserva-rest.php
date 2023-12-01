c2a1a27a31fe630151652ed04621a87d471ab4ae
Kazu N <118866959+Kazu00001@users.noreply.github.com>
2023-12-01 11:26:33 -0600

    mostar reservas resta

---
+++ Expolitec/load-reserva-rest.php
@@ -0,0 +1,89 @@
+<?php
+require('conexion.php');
+
+$columas = ['id_reserva','id_habitacion','id_usuario','fecha_inicio','fecha_fin','Nombres','Apellidos','Tipo_de_habitacion','email','telefono','mascota','Numero_mascotas','Nombre_propetario_target','Numero_de_targeta','Fecha_vencimiento_targeta'];
+$table = "reservas";
+$columas2 = ['id_reserva','Nombres'];
+$campo = isset($_POST['searRev']) ? $Conexion->real_escape_string($_POST['searRev']) : null;
+$where = '';
+
+if ($campo != null) {
+    $where = "WHERE (";
+    $cont = count($columas2);
+
+    for ($i = 0; $i < $cont; $i++) {
+        $where .= $columas2[$i] . " LIKE '%" . $campo . "%' OR ";
+    }
+
+    $where = substr_replace($where, "", -3);
+    $where .= ")";
+}
+
+$consult = "SELECT " . implode(",", $columas) . " FROM $table $where";
+$Conexion->set_charset("utf8");
+header('Content-Type: text/html; charset=utf-8');
+$result = $Conexion->query($consult);
+
+if ($result === false) {
+    die("Error in query: " . $Conexion->error);
+}
+
+$num_rows = $result->num_rows;
+
+if ($num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $row['id_reserva'] . '</td>';
+        echo '<td>' . $row['id_habitacion'] . '</td>';
+        echo '<td>' . $row['id_usuario'] . '</td>';
+        echo '<td>' . $row['fecha_inicio'] . '</td>';
+        echo '<td>' . $row['fecha_fin'] . '</td>';
+        echo '<td>' . $row['Nombres'] . '</td>';
+        echo '<td>' . $row['Apellidos'] . '</td>';
+        echo '<td>' . $row['Tipo_de_habitacion'] . '</td>';
+        echo '<td>' . $row['email'] . '</td>';
+        echo '<td>' . $row['telefono'] . '</td>';
+        echo '<td>' . $row['mascota'] . '</td>';
+        echo '<td>' . $row['Numero_mascotas'] . '</td>';
+        echo '<td>' . $row['Nombre_propetario_target'] . '</td>';
+        echo '<td>' . $row['Numero_de_targeta'] . '</td>';
+        echo '<td>' . $row['Fecha_vencimiento_targeta'] . '</td>';
+        echo '<td> <form action="eliminar.php" method="post">
+        <input type="hidden" name="eliminar" value="'. $row['id_reserva'] . '">
+        <button class="Btn" onclick="enviarFormulario3()">
+  
+        <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+  
+        <div class="text"><pre>   Eliminar</pre></div>
+        </button>
+        
+        </form></td>
+        <script>
+        function enviarFormulario3() {
+            document.getElementById("form3").submit();
+        }
+        </script>
+        </button>'
+        ;
+        echo '<td> <form id="edit" action="edit-resv.php" method="post"> 
+        <button class="button" >
+            Editar 
+            <input type="hidden" name="update" value="'. $row['id_reserva'] . '">
+            <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+                <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+            </svg>
+            </button>
+                    </form> </td> <script>
+                    function enviarFormulario3() {
+                        document.getElementById("edit").submit();
+                    }
+                    </script>'
+                    ;
+                    echo '</tr>';
+    }
+} else {
+    echo '<tr>';
+    echo '<td colspan="17">Sin resultados</td>';
+    echo '</tr>';
+}
+?>
---
+++ Expolitec/most-revers-rest.php
@@ -0,0 +1,103 @@
+<?php
+    require_once('conexion.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lista Empleados</title>
+ 
+    <link rel="stylesheet" href="admin_bar_foot.css">
+    <link rel="stylesheet" href="reservas-css.css">
+    
+    
+</head>
+<body>
+<header>
+        <article id="titfo">
+            <section id="titC">
+                <br>
+                <a href="#"><h1 id="tit">PawTel</h1></a>
+            </section>
+            <section id="barNav">
+            <nav class="nave">
+                <ul class="menu">
+                <li><a href="#">Reservaciones</a>
+                        <div class="topM"></div>
+                        <ul class="menuv">
+                            <li class="list"><a href="reservas-most.php">Mostrar Reservaciones</a></li>
+                            <li class="list"><a href="Tipodehb.php">Mostrar Habitaciones</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Empleados</a>
+                        <div class="topM"></div>
+                        <ul class="menuv">
+                            <li class="list"><a href="empleados.php">Lista de Empleados</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="inventario-mostr.php">Inventario</a></li>
+                </ul>
+
+            </nav>
+            </section>
+        </article>
+    </header>
+    <main>
+        <article id="container">
+            <section id="container_search">
+                <form action="" method="post">
+                    <input type="text" name="searRev" id="searRev" onkeyup="getData()" placeholder="Buscar">
+                </form>
+                <form action="form_addEmp.html" method="post">
+                    <input type="submit" value="Nuevo producto" id="addEmp">
+                </form>
+            </section>
+            <section id="content_invent2">
+                <table>
+                    <thead>
+                    <th>id_reserva</th>
+                    <th>id_habitacion</th>
+                    <th>id_usuario</th>
+                    <th>Inicio de estancia</th>
+                    <th>Fin de estancia</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Tipo de habitacion</th>
+                    <th>Email</th>
+                    <th>Telfono</th>
+                    <th>Mascota</th>
+                    <th>No. M</th>
+                    <th>Nombre propetario target</th>
+                    <th>Numero de targeta</th>
+                    <th>Fecha vencimiento targeta</th>
+                    <th colspan="2">Otras</th>
+                    </thead>
+                    <tbody id="content_invent"></tbody>
+                <script>
+                document.addEventListener("DOMContentLoaded", getData);
+
+                function getData() {
+                    let input = document.getElementById("searRev").value;
+                    let content = document.getElementById("content_invent");
+                    let url = "reservas-load.php";
+                    let formData = new FormData();
+                    formData.append('searRev', input);
+
+                        fetch(url, {
+                                method: "POST",
+                                body: formData
+                            }).then(response => response.text())
+                                .then(data => {
+                                console.log(data);
+                                content.innerHTML = data;
+                                }).catch(err => console.log(err));
+                            }
+                        </script>
+                </table>
+            </section>
+
+<!-------------------------------------------------------------------------->
+        </article>
+    </main>
+</body>
+</html>
